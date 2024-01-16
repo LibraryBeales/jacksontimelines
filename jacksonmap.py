a20b836bb997d14b7b82bb6adf39af44a589eb88
LibraryBeales <rdbeales@gmail.com>
2024-01-16 12:42:00 -0500

    12344312

---
+++ jacksonmap.py
@@ -0,0 +1,29 @@
+import url
+import requests
+
+
+df = pd.DataFrame([
+    dict(Name="Ann D. Godfrey", Loc="Godfrey's Ferry, SC", Start='1829', Finish='1832', Admin='Jackson Administration 1 ,Jackson Administration'),
+    dict(Name="Ann H. Richards", Loc="Joanne Furnace, PA", Start='1833', Finish='1844', Admin='Jackson Administration,Harrison / Tyler Administration'),
+    dict(Name="Betsy S. Wilson", Loc="Ten Mile Stand, TN", Start='1829', Finish='1832', Admin='Jackson Administration,Jackson Administration'),
+    dict(Name="Catharine M. Browner", Loc="New Windsor, MD", Start='1833', Finish='1844', Admin='Jackson Administration,Harrison / Tyler Administration'),
+    dict(Name="Catherine Ann Canfield", Loc="New Philadelphia, OH", Start='1829', Finish='1848', Admin='Jackson Administration,Polk Administration'),
+    dict(Name="Catherine Graham", Loc="Port Republic, VA", Start='1833', Finish='1840', Admin='Jackson Administration,Van Buren Administration'),
+    dict(Name="Elizabeth Coffin Hoggatt", Loc="Paoli, IN", Start='1833', Finish='1836', Admin='Jackson Administration,Jackson Administration'),
+    dict(Name="Elizabeth Meyers Lauman", Loc="Middletown, PA", Start='1833', Finish='1836', Admin='Jackson Administration,Jackson Administration'),
+    dict(Name="Freeborn Rider", Loc="Rice City, RI", Start='1833', Finish='1840', Admin='Jackson Administration,Van Buren Administration'),
+    dict(Name="Jane Compton", Loc="Manchester, SC", Start='1833', Finish='1840', Admin='Jackson Administration,Van Buren Administration'),
+    dict(Name="Jane Dick", Loc="Dicks Mills, OH", Start='1829', Finish='1836', Admin='Jackson Administration,Jackson Administration'),
+    dict(Name="Margaret J. Walters", Loc="Lewistown, PA", Start='1829', Finish='1836', Admin='Jackson Administration,Jackson Administration'),
+    dict(Name="Margaret Johnston", Loc="Michigantown, IN", Start='1833', Finish='1840', Admin='Jackson Administration,Van Buren Administration'),
+    dict(Name="Margaret M. Reid", Loc="Cotocton, MD", Start='1833', Finish='1836', Admin='Jackson Administration,Jackson Administration'),
+    dict(Name="Mary Odenheimer Deshong", Loc="Chester, PA", Start='1829', Finish='1840', Admin='Jackson Administration,Van Buren Administration'),
+    dict(Name="Mary Dickson", Loc="Lancaster, PA", Start='1829', Finish='1852', Admin='Jackson Administration,Taylor / Fillmore Administration'),
+    dict(Name="Mary M. Hayes", Loc="Hayesville, NC", Start='1833', Finish='1844', Admin='Jackson Administration,Harrison/Tyler Administration'),
+    dict(Name="Mary McKain", Loc="High Rock, NC", Start='1833', Finish='1836', Admin='Jackson Administration,Jackson Administration'),
+    dict(Name="Rebecca Jewett Gay", Loc="Washington Hollow, NY", Start='1833', Finish='1836', Admin='Jackson Administration,Jackson Administration'),
+    dict(Name="Rebecca Sanders", Loc="Leesburg, OH", Start='1833', Finish='1836', Admin='Jackson Administration,Jackson Administration'),
+    dict(Name="Sarah A. Cunningham", Loc="New London Crossroads, PA", Start='1833', Finish='1844', Admin='Jackson Administration,Harrison/Tyler Administration'),
+    dict(Name="Sarah Davis", Loc="Paoli, PA", Start='1833', Finish='1848', Admin='Jackson Administration,Polk Administration'),
+    dict(Name="Sarah Pryor", Loc="Pryors Vale, VA", Start='1833', Finish='1840', Admin='Jackson Administration,Van Buren Administration')
+])
--- jacksontimeline.py
+++ jacksontimeline.py
@@ -3,8 +3,63 @@ import plotly.graph_objects as go
 import pandas as pd
 from plotly.subplots import make_subplots
 
+df_new = pd.DataFrame([
+    dict(Name="Ann D. Godfrey", Loc="Godfrey's Ferry, SC", Start='1829', Finish='1833', Admin='First Jackson Administration'),
+    dict(Name="Ann H. Richards", Loc="Joanne Furnace, PA", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Ann H. Richards", Loc="Joanne Furnace, PA", Start='1837', Finish='1841', Admin='Van Buren Administration'),
+    dict(Name="Ann H. Richards", Loc="Joanne Furnace, PA", Start='1841', Finish='1845', Admin='Harrison/Tyler Administration'),
+    dict(Name="Betsy S. Wilson", Loc="Ten Mile Stand, TN", Start='1829', Finish='1833', Admin='First Jackson Administration'),
+    dict(Name="Catharine M. Browner", Loc="New Windsor, MD", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Catharine M. Browner", Loc="New Windsor, MD", Start='1837', Finish='1841', Admin='Van Buren Administration'),
+    dict(Name="Catharine M. Browner", Loc="New Windsor, MD", Start='1841', Finish='1845', Admin='Harrison/Tyler Administration'),
+    dict(Name="Catherine Ann Canfield", Loc="New Philadelphia, OH", Start='1829', Finish='1833', Admin='First Jackson Administration'),
+    dict(Name="Catherine Ann Canfield", Loc="New Philadelphia, OH", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Catherine Ann Canfield", Loc="New Philadelphia, OH", Start='1837', Finish='1841', Admin='Van Buren Administration'),
+    dict(Name="Catherine Ann Canfield", Loc="New Philadelphia, OH", Start='1841', Finish='1845', Admin='Harrison/Tyler Administration'),
+    dict(Name="Catherine Ann Canfield", Loc="New Philadelphia, OH", Start='1845', Finish='1849', Admin='Polk Administration'),
+    dict(Name="Catherine Graham", Loc="Port Republic, VA", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Catherine Graham", Loc="Port Republic, VA", Start='1837', Finish='1841', Admin='Van Buren Administration'),
+    dict(Name="Elizabeth Coffin Hoggatt", Loc="Paoli, IN", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Elizabeth Meyers Lauman", Loc="Middletown, PA", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Freeborn Rider", Loc="Rice City, RI", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Freeborn Rider", Loc="Rice City, RI", Start='1837', Finish='1841', Admin='Van Buren Administration'),
+    dict(Name="Jane Compton", Loc="Manchester, SC", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Jane Compton", Loc="Manchester, SC", Start='1837', Finish='1841', Admin='Van Buren Administration'),
+    dict(Name="Jane Dick", Loc="Dicks Mills, OH", Start='1829', Finish='1833', Admin='First Jackson Administration'),
+    dict(Name="Jane Dick", Loc="Dicks Mills, OH", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Margaret J. Walters", Loc="Lewistown, PA", Start='1829', Finish='1833', Admin='First Jackson Administration'),
+    dict(Name="Margaret J. Walters", Loc="Lewistown, PA", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Margaret Johnston", Loc="Michigantown, IN", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Margaret Johnston", Loc="Michigantown, IN", Start='1837', Finish='1841', Admin='Van Buren Administration'),
+    dict(Name="Margaret M. Reid", Loc="Cotocton, MD", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Ann D. Godfrey", Loc="Godfrey's Ferry, SC", Start='1829', Finish='1833', Admin='First Jackson Administration'),
+    dict(Name="Mary Odenheimer Deshong", Loc="Chester, PA", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Mary Odenheimer Deshong", Loc="Chester, PA", Start='1837', Finish='1841', Admin='Van Buren Administration'),
+    dict(Name="Mary Dickson", Loc="Lancaster, PA", Start='1829', Finish='1833', Admin='First Jackson Administration'),
+    dict(Name="Mary Dickson", Loc="Lancaster, PA", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Mary Dickson", Loc="Lancaster, PA", Start='1837', Finish='1841', Admin='Van Buren Administration'),
+    dict(Name="Mary Dickson", Loc="Lancaster, PA", Start='1841', Finish='1845', Admin='Harrison/Tyler Administration'),
+    dict(Name="Mary Dickson", Loc="Lancaster, PA", Start='1845', Finish='1849', Admin='Polk Administration'),
+    dict(Name="Mary Dickson", Loc="Lancaster, PA", Start='1849', Finish='1853', Admin='Taylor/Fillmore Administration'),
+    dict(Name="Mary M. Hayes", Loc="Hayesville, NC", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Mary M. Hayes", Loc="Hayesville, NC", Start='1837', Finish='1841', Admin='Van Buren Administration'),
+    dict(Name="Mary M. Hayes", Loc="Hayesville, NC", Start='1841', Finish='1845', Admin='Harrison/Tyler Administration'),
+    dict(Name="Mary McKain", Loc="High Rock, NC", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Rebecca Jewett Gay", Loc="Washington Hollow, NY", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Rebecca Sanders", Loc="Leesburg, OH", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Sarah A. Cunningham", Loc="New London Crossroads, PA", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Sarah A. Cunningham", Loc="New London Crossroads, PA", Start='1837', Finish='1841', Admin='Van Buren Administration'),
+    dict(Name="Sarah A. Cunningham", Loc="New London Crossroads, PA", Start='1841', Finish='1845', Admin='Harrison/Tyler Administration'),
+    dict(Name="Sarah Davis", Loc="Paoli, PA", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Sarah Davis", Loc="Paoli, PA", Start='1837', Finish='1841', Admin='Van Buren Administration'),
+    dict(Name="Sarah Davis", Loc="Paoli, PA", Start='1841', Finish='1845', Admin='Harrison/Tyler Administration'),
+    dict(Name="Sarah Davis", Loc="Paoli, PA", Start='1845', Finish='1849', Admin='Polk Administration'),
+    dict(Name="Sarah Pryor", Loc="Pryors Vale, VA", Start='1833', Finish='1837', Admin='Second Jackson Administration'),
+    dict(Name="Sarah Pryor", Loc="Pryors Vale, VA", Start='1837', Finish='1841', Admin='Van Buren Administration')
+])
+
 df = pd.DataFrame([
-    dict(Name="Ann D. Godfrey", Loc="Godfrey's Ferry, SC", Start='1829', Finish='1832', Admin='Jackson Administration,Jackson Administration'),
+    dict(Name="Ann D. Godfrey", Loc="Godfrey's Ferry, SC", Start='1829', Finish='1832', Admin='Jackson Administration 1 ,Jackson Administration'),
     dict(Name="Ann H. Richards", Loc="Joanne Furnace, PA", Start='1833', Finish='1844', Admin='Jackson Administration,Harrison / Tyler Administration'),
     dict(Name="Betsy S. Wilson", Loc="Ten Mile Stand, TN", Start='1829', Finish='1832', Admin='Jackson Administration,Jackson Administration'),
     dict(Name="Catharine M. Browner", Loc="New Windsor, MD", Start='1833', Finish='1844', Admin='Jackson Administration,Harrison / Tyler Administration'),
@@ -19,17 +74,12 @@ df = pd.DataFrame([
     dict(Name="Margaret Johnston", Loc="Michigantown, IN", Start='1833', Finish='1840', Admin='Jackson Administration,Van Buren Administration'),
     dict(Name="Margaret M. Reid", Loc="Cotocton, MD", Start='1833', Finish='1836', Admin='Jackson Administration,Jackson Administration'),
     dict(Name="Mary Odenheimer Deshong", Loc="Chester, PA", Start='1829', Finish='1840', Admin='Jackson Administration,Van Buren Administration'),
-    #dict(Name="Mary Dickson", Loc="Lancaster, PA", Start='1829', Finish='1852', Admin='Jackson Administration,Taylor / Fillmore Administration'),
-    dict(Name="Mary Dickson", Loc="Lancaster, PA", Start='1829', Finish='1837', Admin='Jackson Administration'),
-    dict(Name="Mary Dickson", Loc="Lancaster, PA", Start='1837', Finish='1841', Admin='Van Buren Administration'),
-    dict(Name="Mary Dickson", Loc="Lancaster, PA", Start='1841', Finish='1845', Admin='Harrison/Tyler Administration'),
-    dict(Name="Mary Dickson", Loc="Lancaster, PA", Start='1845', Finish='1849', Admin='Polk Administration'),
-    dict(Name="Mary Dickson", Loc="Lancaster, PA", Start='1849', Finish='1853', Admin='Taylor/Fillmore Administration'),
-    dict(Name="Mary M. Hayes", Loc="Hayesville, NC", Start='1833', Finish='1844', Admin='Jackson Administration,Harrison / Tyler Administration'),
+    dict(Name="Mary Dickson", Loc="Lancaster, PA", Start='1829', Finish='1852', Admin='Jackson Administration,Taylor / Fillmore Administration'),
+    dict(Name="Mary M. Hayes", Loc="Hayesville, NC", Start='1833', Finish='1844', Admin='Jackson Administration,Harrison/Tyler Administration'),
     dict(Name="Mary McKain", Loc="High Rock, NC", Start='1833', Finish='1836', Admin='Jackson Administration,Jackson Administration'),
     dict(Name="Rebecca Jewett Gay", Loc="Washington Hollow, NY", Start='1833', Finish='1836', Admin='Jackson Administration,Jackson Administration'),
     dict(Name="Rebecca Sanders", Loc="Leesburg, OH", Start='1833', Finish='1836', Admin='Jackson Administration,Jackson Administration'),
-    dict(Name="Sarah A. Cunningham", Loc="New London Crossroads, PA", Start='1833', Finish='1844', Admin='Jackson Administration,Harrison / Tyler Administration'),
+    dict(Name="Sarah A. Cunningham", Loc="New London Crossroads, PA", Start='1833', Finish='1844', Admin='Jackson Administration,Harrison/Tyler Administration'),
     dict(Name="Sarah Davis", Loc="Paoli, PA", Start='1833', Finish='1848', Admin='Jackson Administration,Polk Administration'),
     dict(Name="Sarah Pryor", Loc="Pryors Vale, VA", Start='1833', Finish='1840', Admin='Jackson Administration,Van Buren Administration')
 ])
@@ -43,7 +93,7 @@ df2 = pd.DataFrame([
     dict(Name="Fillmore Administration", Start='1851', Finish='1852')
 ])
 
-fig = px.timeline(df, x_start="Start", x_end="Finish", y="Name", color="Admin")
+fig = px.timeline(df_new, x_start="Start", x_end="Finish", y="Name", color="Admin")
 
 '''
 fig = make_subplots(specs=[[{"secondary_y": True}]])
